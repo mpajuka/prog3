27e853cef82116297810ca953aa9301e82c51608
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-11-03 18:51:59 +0200

    remove f to c

--- Round10/GIT_EX/temperature.py
+++ Round10/GIT_EX/temperature.py
@@ -9,7 +9,6 @@ def main():
 
 
     print("{} C = {} F".format(input_temp, t_transform(input_temp, 'c')))
-    print("{} F = {} C".format(input_temp, t_transform(input_temp, 'f')))
 
 
 
@@ -17,8 +16,6 @@ def t_transform(temp, type):
 
     if type == 'c':
         return float((temp * 9/5) + 32) 
-    if type == 'f':
-        return float((temp - 32) * 5/9)
     return None
 
 
