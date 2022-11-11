928dc33735dfb2caf490c4bed9e8d062a10fd974
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-11-11 20:05:29 +0200

    update junitorder

--- Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 public class OrderTests {
     
     @Test
-    public void testIllegalPriceAndName() {
+    public void testIllegalName() {
         Exception thrown = assertThrows(IllegalArgumentException.class,
                 () -> {new Order.Item(null, 1234.12);
                 });
+    }
+    
+    @Test
+    public void testIllegalPrice() {
         Exception thrown2 = assertThrows(IllegalArgumentException.class,
                 () -> {new Order.Item("test", -1); 
                 });
@@ -81,5 +85,26 @@ public class OrderTests {
         assertEquals(expected, instance.toString());
     }
    
+    @Test
+    public void testAddItemsIllegalCount() {
+        Order order = new Order();
+        assertThrows(IllegalArgumentException.class, () ->
+        {
+           order.addItems(new Order.Item("foobar", 1.0), -1);
+        });
+    }
+    
+    @Test
+    public void testAdditemsDuplicate() {
+        Order order = new Order();
+        order.addItems(new Order.Item("foobar", 1.0), 1);
+        Order.Item duplicate = new Order.Item("foobar", 2.0);
+        
+        assertThrows(IllegalStateException.class, () ->
+        {
+           order.addItems(duplicate, 1);
+        });
+    }
+    
     
 }
