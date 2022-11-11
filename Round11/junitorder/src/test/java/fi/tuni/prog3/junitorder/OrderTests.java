bc3dfdd434b5a35ac7e739e6a6b0385e3252e42e
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-11-11 21:04:04 +0200

    remove unnecessary

--- Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
@@ -1,7 +1,5 @@
 package fi.tuni.prog3.junitorder;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.NoSuchElementException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
