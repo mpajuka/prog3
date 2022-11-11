020df61ca229773678975cbbfd8e52fc416de892
Miska Pajukangas <56785774+miskapajj@users.noreply.github.com>
2022-11-11 19:46:26 +0200

    add junitorder

--- Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
+++ Round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderTests.java
@@ -1,15 +1,85 @@
 package fi.tuni.prog3.junitorder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class OrderTests {
     
     @Test
-    public void testIllegalPrice() {
+    public void testIllegalPriceAndName() {
         Exception thrown = assertThrows(IllegalArgumentException.class,
-                () -> {new Order.Item(null, 1234);
-                
-        });
+                () -> {new Order.Item(null, 1234.12);
+                });
+        Exception thrown2 = assertThrows(IllegalArgumentException.class,
+                () -> {new Order.Item("test", -1); 
+                });
     }
+    
+    @Test
+    public void testGetPrice() {
+        Order.Item instance = new Order.Item("test", 1234.12);
+        double expected = 1234.12;
+        assertEquals(expected, instance.getPrice());
+    }
+    
+    @Test
+    public void testGetName() {
+        Order.Item instance = new Order.Item("test", 1234.12);
+        String expected = "test";
+        assertEquals(expected, instance.getName());
+    }
+    
+    @Test
+    public void testToString() {
+        Order.Item instance = new Order.Item("test", 1234.12);
+        String expected = "Item(test,1234.12)";
+        assertEquals(expected, instance.toString());
+    }
+    
+    @Test
+    public void testEquals() {
+        Order.Item instance = new Order.Item("test", 1234.12);
+        Order.Item instance2 = new Order.Item("foobar", 4321.21);
+        assertTrue(instance.equals(instance));
+        assertFalse(instance.equals(instance2));
+    }
+    
+    @Test
+    public void testIllegalUnitCount() {
+        Exception thrown = assertThrows(IllegalArgumentException.class,
+                () -> {new Order.Entry(new Order.Item("test", 1234.12), -1);
+                });
+    }
+    
+    @Test
+    public void testGetItemName() {
+        Order.Entry instance = new Order.Entry(new Order.Item("a", 1.0), 1);
+        String expected = "a";
+        assertEquals(expected, instance.getItemName());
+    }
+    
+    @Test
+    public void testGetUnitPrice() {
+        Order.Entry instance = new Order.Entry(new Order.Item("a", 1.0), 1);
+        double expected = 1.0;
+        assertEquals(expected, instance.getUnitPrice());
+    }
+    
+    @Test
+    public void testGetItem() {
+        Order.Entry instance = new Order.Entry(new Order.Item("a", 1.0), 1);
+        assertEquals(new Order.Item("a", 1.0), instance.getItem());
+    }
+    
+    @Test
+    public void testEntryToString() {
+        Order.Entry instance = new Order.Entry(new Order.Item("a", 1.0), 1);
+        String expected = "1 units of a";
+        assertEquals(expected, instance.toString());
+    }
+   
+    
 }
